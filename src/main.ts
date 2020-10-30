d673ea67d1f359a45f8e5d681063d6531e4a8fee
EnochQin <qyqdream@gmail.com>
2020-10-30 20:37:23 +0800

    add: index.scss Universal style

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
-import './index.css';
+import './index.scss';
 import { createWebHashHistory, createRouter } from 'vue-router';
 
 const history = createWebHashHistory();
