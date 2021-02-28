cdf4a9ea24ab671509efe8a5de81ef004ee61787
EnochQin <qyqdream@gmail.com>
2021-02-28 17:47:09 +0800

    add component Topnav

---
+++ src/components/Topnov.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="topnav">
+    <div class="logo">logo</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+<style lang="scss" scoped>
+nav.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <nav class="topnav">
-      <div class="logo">logo</div>
-      <div class="menu">
-        <ul>
-          <li>菜单1</li>
-          <li>菜单2</li>
-        </ul>
-      </div>
-    </nav>
+    <Topnav />
     <div class="banner">
       <h1>V3-Wheel</h1>
       <h2>一个基于 Vue3 和 TypeScript 的组件库</h2>
@@ -20,22 +12,11 @@
   </div>
 </template>
 
+<script lang="ts">
+import Topnav from "../components/Topnov.vue";
+export default {
+  components: { Topnav },
+};
+</script>
 <style lang="scss" scoped>
-nav.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
 </style>
