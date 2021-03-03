e75c2618392798d297d5accde5e673a561fd2844
EnochQin <qyqdream@gmail.com>
2021-03-03 23:01:32 +0800

    Switch component add props

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h1>Switch 文档</h1>
-    <Switch />
+    <Switch :value="defaultValue" @input="defaultValue = $event" />
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 export default {
   components: { Switch },
+  setup() {
+    const defaultValue = ref(false);
+
+    return { defaultValue };
+  },
 };
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,16 +1,21 @@
 <template>
-  <button @click="toggle" :class="{ checked }"><span></span></button>
+  <button @click="toggle" :class="{ checked: value }"><span></span></button>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 export default {
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit("input", !props.value);
     };
-    return { checked, toggle };
+    return { toggle };
   },
 };
 </script>
