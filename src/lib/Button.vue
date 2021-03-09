d2c80b18a2c3b1bbb77ed41161f8d8a5aa694294
EnochQin <qyqdream@gmail.com>
2021-03-09 22:37:27 +0800

    Button: add theme style

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="v3wheel-button" :class="`theme-${theme}`"><slot /></button>
+  <button class="v3wheel-button" :class="`v3wheel-theme-${theme}`">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
@@ -12,3 +14,58 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.v3wheel-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.v3wheel-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.v3wheel-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+}
+</style>
