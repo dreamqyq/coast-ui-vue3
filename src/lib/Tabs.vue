39e562a03c288cd964da05cdec5a4a04410bcb99
EnochQin <qyqdream@gmail.com>
2021-03-15 22:56:51 +0800

    Tabs: nav style and render TabsPanel's title

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,18 @@
 <template>
-  <component v-for="(tab, index) in defaults" :is="tab" :key="index" />
+  <div class="v3wheel-tabs">
+    <div class="v3wheel-tabs-nav">
+      <div
+        class="v3wheel-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="v3wheel-tabs-content">
+      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,7 +25,35 @@ export default {
         throw new Error("Tabs 的子标签必须为 TabsPanel");
       }
     });
-    return { defaults };
+    const titles = defaults.map((tag) => tag.props.title);
+    return { defaults, titles };
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.v3wheel-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
