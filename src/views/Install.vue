435f62604aae97e11905037ab06084674de648f7
EnochQin <qyqdream@gmail.com>
2021-03-30 21:50:25 +0800

    doc page content: use markdown

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,16 +1,16 @@
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行下列命令：</p>
-    <pre><code>npm install coast-ui-vue3</code></pre>
-    <p>或</p>
-    <pre><code>yarn add coast-ui-vue3</code></pre>
-
-    <p>下一节：<a href="#/doc/get-start">开始使用</a></p>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
 
-<script lang="ts">
+<script lang="js">
+import md from "../markdown/Install.md";
+export default {
+  data(){
+    return{
+      md
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
