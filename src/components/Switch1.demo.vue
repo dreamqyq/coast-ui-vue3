5fa88d142e50dd82612a51c0c446632f1aec6f8f
EnochQin <qyqdream@gmail.com>
2021-03-31 22:53:51 +0800

    add Switch demo file

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,19 @@
+<demo>
+  常规用法
+</demo>
+
+<template>
+  <Switch v-model:value="defaultValue" />
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+  setup() {
+    const defaultValue = ref(false);
+    return { defaultValue };
+  },
+};
+</script>
---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,19 @@
+<demo>
+  支持 disabled
+</demo>
+
+<template>
+  <Switch v-model:value="defaultValue" disabled />
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+  setup() {
+    const defaultValue = ref(false);
+    return { defaultValue };
+  },
+};
+</script>
