7d316ee6dff9ce94f8ee9c5a71cfde51f8a1eceb
EnochQin <qyqdream@gmail.com>
2021-03-31 22:54:36 +0800

    Switch doc: add demo code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,19 +1,73 @@
 <template>
   <div>
     <h1>Switch 文档</h1>
-    <Switch v-model:value="defaultValue" />
+    <div class="demo">
+      <h2>{{ SwitchDemo1.__sourceCodeTitle }}</h2>
+      <div class="demo-component">
+        <component :is="SwitchDemo1" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{ SwitchDemo1.__sourceCode }}</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>{{ SwitchDemo2.__sourceCodeTitle }}</h2>
+      <div class="demo-component">
+        <component :is="SwitchDemo2" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{ SwitchDemo2.__sourceCode }}</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
+import SwitchDemo1 from "../components/Switch1.demo.vue";
+import SwitchDemo2 from "../components/Switch2.demo.vue";
+import Button from "../lib/Button.vue";
 export default {
-  components: { Switch },
+  components: { Button },
   setup() {
-    const defaultValue = ref(false);
-
-    return { defaultValue };
+    return {
+      SwitchDemo1,
+      SwitchDemo2,
+    };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
 export default {
   props: {
     value: {
