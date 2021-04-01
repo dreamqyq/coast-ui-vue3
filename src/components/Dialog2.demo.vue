2687cdf283e9bda77130320b1bb8705eee284e1a
EnochQin <qyqdream@gmail.com>
2021-04-01 17:11:46 +0800

    doc: finish Dialog 2 demo files

---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,37 @@
+<demo>一键打开 Dialog</demo>
+
+<template>
+  <div>
+    <Button @click="showDialog">打开对话框</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+import { openDialog } from '../lib/openDialog';
+
+export default {
+  components: {
+    Button
+  },
+  setup() {
+    const showDialog = () => {
+      openDialog({
+        content: '文档内容',
+        title: '标题',
+        overlayClosable: false,
+        confirm() {
+          console.log('confirm');
+          return false;
+        },
+        cancel() {
+          console.log('cancel');
+        },
+      });
+    };
+    return {
+      showDialog
+    };
+  }
+};
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,53 +1,21 @@
 <template>
   <h1>Button 文档</h1>
-  <h2>示例1：基本用法</h2>
-  <Button @click="toggle">打开 Dialog</Button>
-  <Dialog v-model:visible="visible" :cancel="cancel" :confirm="confirm" :overlayClosable="false">
-    <template v-slot:content>
-      <p>第一行内容</p>
-      <p>第二行内容</p>
-    </template>
-  </Dialog>
-
-  <h2>示例2：openDialog 方法</h2>
-  <Button @click="showDialog">openDialog 打开 Dialog</Button>
+  <Demo :component="DialogDemo1" />
+  <Demo :component="DialogDemo2" />
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import { openDialog } from '../lib/openDialog';
+import Demo from './Demo.vue';
+import DialogDemo1 from '../components/Dialog1.demo.vue';
+import DialogDemo2 from '../components/Dialog2.demo.vue';
 
 export default {
-  components: { Button, Dialog },
+  components: { Demo },
   setup() {
-    const visible = ref(false);
-    const toggle = () => {
-      visible.value = !visible.value;
+    return {
+      DialogDemo1,
+      DialogDemo2
     };
-    const confirm = () => {
-      console.log('confirm click');
-      return false;
-    };
-    const cancel = () => {
-      console.log('cancel click');
-    };
-
-    const showDialog = () => {
-      openDialog({
-        content: 'Dialog content',
-        title: 'Dialog title',
-        confirm() {
-          console.log('confirm');
-          return false;
-        },
-        cancel() {
-          console.log('cancel');
-        },
-      });
-    };
-    return { visible, toggle, confirm, cancel, showDialog };
   },
 };
 </script>
