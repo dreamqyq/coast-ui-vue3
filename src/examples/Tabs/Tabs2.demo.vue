729f8555e12eed225dc9b228e45da231f3c0b1dc
EnochQin <qyqdream@gmail.com>
2021-04-30 23:35:18 +0800

    doc: add Tabs2.demo.vue: TabPanel disabled

---
+++ src/examples/Tabs/Tabs2.demo.vue
@@ -0,0 +1,24 @@
+<demo>TabPanel 支持 disabled</demo>
+
+<template>
+  <Tabs v-model:selected="selected">
+    <TabPanel title="导航1">内容1</TabPanel>
+    <TabPanel title="导航2 disabled" :disabled="true">内容2</TabPanel>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import { TabPanel, Tabs } from '../../lib/index';
+import { ref } from 'vue';
+
+export default {
+  components: {
+    Tabs,
+    TabPanel,
+  },
+  setup() {
+    const selected = ref('导航1');
+    return { selected };
+  },
+};
+</script>
--- src/examples/Tabs/TabsDemo.vue
+++ src/examples/Tabs/TabsDemo.vue
@@ -2,19 +2,21 @@
   <div>
     <h1>Tabs 文档</h1>
     <Demo :component="TabsDemo1" />
+    <Demo :component="TabsDemo2" />
   </div>
 </template>
 
 <script lang="ts">
 import Demo from '../../components/Demo.vue';
 import TabsDemo1 from './Tabs1.demo.vue';
+import TabsDemo2 from './Tabs2.demo.vue';
 
 export default {
   components: {
     Demo,
   },
   setup() {
-    return { TabsDemo1 };
+    return { TabsDemo1, TabsDemo2 };
   },
 };
 </script>
