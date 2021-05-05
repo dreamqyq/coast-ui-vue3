143c12c8ddb4c68070fae6f4730b40c860249cd5
EnochQin <qyqdream@gmail.com>
2021-05-05 23:16:15 +0800

    chore: vite to 2.2.4, refactor demo parse

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
 import { md } from './plugins/md';
 import fs from 'fs';
-import { baseParse } from '@vue/compiler-core';
+import { baseParse, ElementNode } from '@vue/compiler-core';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-export default {
-  assetsDir: 'assets',
-  base: './',
-  plugins: [md()],
-  vueCustomBlockTransforms: {
-    demo: options => {
-      const { code, path } = options;
-      const file = fs.readFileSync(path).toString();
-      const parsed = baseParse(file).children.find(n => n.tag === 'demo');
-      const title = parsed.children[0].content;
-      const main = file.split(parsed.loc.source).join('').trim();
-      return `export default function (Component) {
-        Component.__sourceCode = ${JSON.stringify(main)}
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
-      }`.trim();
-    }
-  }
+type FindElementType = {
+  tag: string;
+} & ElementNode;
+const coastDemoParse = {
+  name: 'coast-demo-parse',
+  transform(code: string, id: string) {
+    if (!/vue&type=demo/.test(id)) return;
+    const file = fs.readFileSync(id.split('?')[0]).toString();
+    const parsed = baseParse(file).children.find((n: FindElementType) => n.tag === 'demo');
+    const main = file.split(parsed.loc.source).join('').trim();
+    const sourceCode = main;
+    return `export default Comp => {
+      Comp.__sourceCodeTitle = ${JSON.stringify(code)}
+      Comp.__sourceCode = ${JSON.stringify(sourceCode)}
+    }`;
+  },
 };
+
+export default defineConfig({
+  build: {
+    assetsDir: 'assets',
+  },
+  base: './',
+  plugins: [md(), vue(), coastDemoParse],
+});
