120219b18a4862147f8280123119eba999eecf05
EnochQin <qyqdream@gmail.com>
2021-05-05 23:15:40 +0800

    chore: vite to 2.2.4, refactor md plugin

--- plugins/md.ts
+++ plugins/md.ts
@@ -1,35 +1,25 @@
-import path from 'path';
-import fs from 'fs';
+import type { Plugin } from 'vite';
 import marked from 'marked';
 
-const mdToJs = str => {
+const mdToJs = (str: string) => {
   const content = JSON.stringify(marked(str));
   return `export default ${content}`;
 };
 
-export function md() {
-  return {
-    configureServer: [
-      // 用于开发
-      async ({ app }) => {
-        app.use(async (ctx, next) => {
-          // koa
-          if (ctx.path.endsWith('.md')) {
-            ctx.type = 'js';
-            const filePath = path.join(process.cwd(), ctx.path);
-            ctx.body = mdToJs(fs.readFileSync(filePath).toString());
-          } else {
-            await next();
-          }
-        });
-      }
-    ],
-    transforms: [
-      {
-        // 用于 rollup // 插件
-        test: context => context.path.endsWith('.md'),
-        transform: ({ code }) => mdToJs(code)
-      }
-    ]
-  };
-}
+export const md = (): Plugin => ({
+  name: 'md-plugin',
+  async handleHotUpdate(ctx) {
+    // 用于开发
+    if (ctx.file.endsWith('.md')) {
+      ctx.read = async function () {
+        return mdToJs(await ctx.read());
+      };
+    }
+  },
+  transform(raw, id) {
+    // 用于 rollup // 插件
+    if (id.endsWith('md')) {
+      return { code: mdToJs(raw) };
+    }
+  },
+});
