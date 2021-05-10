8910960f6808f6821e266a658265270b1107b9bb
EnochQin <qyqdream@gmail.com>
2021-05-10 23:32:34 +0800

    website: add AsideNav component

---
+++ src/components/AsideNav.vue
@@ -0,0 +1,96 @@
+<template>
+  <aside :class="{ visible }">
+    <h2>文档</h2>
+    <ol>
+      <li>
+        <router-link to="/doc/introduce">介绍</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/install">安装</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/get-start">开始</router-link>
+      </li>
+    </ol>
+    <h2>组件列表</h2>
+    <ol>
+      <li v-for="nav in demoViewList" :key="nav.title">
+        <router-link :to="`/doc/${nav.path}`">{{ nav.title }} 组件</router-link>
+      </li>
+    </ol>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { demoViewList } from '../router/exampleDocsRouter';
+
+export default defineComponent({
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup() {
+    return { demoViewList };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$asideIndex: 10;
+aside {
+  background: #fff;
+  width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  z-index: $asideIndex;
+  transition: all 0.25s;
+  opacity: 0;
+  transform: translateX(-100%);
+
+  @media (max-width: 500px) {
+    box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.1);
+  }
+
+  &.visible {
+    transform: translateX(0);
+    opacity: 1;
+  }
+
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+  }
+
+  > h2 {
+    text-align: left;
+    padding: 4px 1em;
+  }
+
+  > ol {
+    li {
+      text-align: left;
+      margin: 4px 0;
+
+      > a {
+        display: block;
+        padding: 8px 1.5em;
+      }
+
+      .router-link-active {
+        background: #fff;
+        color: #409eff;
+        text-decoration: underline;
+        font-weight: 600;
+      }
+    }
+  }
+}
+</style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,26 +2,7 @@
   <div class="layout">
     <TopNav class="nav" toggleAsideBtnVisible />
     <div class="content">
-      <aside :class="{ visible: asideVisible }">
-        <h2>文档</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/introduce">介绍</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/install">安装</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/get-start">开始</router-link>
-          </li>
-        </ol>
-        <h2>组件列表</h2>
-        <ol>
-          <li v-for="nav in demoViewList" :key="nav.title">
-            <router-link :to="`/doc/${nav.path}`">{{ nav.title }} 组件</router-link>
-          </li>
-        </ol>
-      </aside>
+      <AsideNav :visible="asideVisible" />
       <main>
         <router-view />
       </main>
@@ -31,20 +12,19 @@
 
 <script lang="ts">
 import { inject, Ref } from 'vue';
+import AsideNav from '../components/AsideNav.vue';
 import TopNav from '../components/TopNav.vue';
-import { demoViewList } from '../router/exampleDocsRouter';
 
 export default {
-  components: { TopNav },
+  components: { AsideNav, TopNav },
   setup() {
     const asideVisible = inject<Ref<boolean>>('asideVisible');
-    return { asideVisible, demoViewList };
+    return { asideVisible };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-$asideIndex: 10;
 .layout {
   display: flex;
   flex-direction: column;
@@ -74,60 +54,6 @@ $asideIndex: 10;
   > main {
     flex-grow: 1;
     padding: 16px;
-  }
-}
-
-aside {
-  background: #fff;
-  width: 150px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-  z-index: $asideIndex;
-  transition: all 0.25s;
-  opacity: 0;
-  transform: translateX(-100%);
-
-  @media (max-width: 500px) {
-    box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.1);
-  }
-
-  &.visible {
-    transform: translateX(0);
-    opacity: 1;
-  }
-
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-  }
-
-  > h2 {
-    text-align: left;
-    padding: 4px 1em;
-  }
-
-  > ol {
-    li {
-      text-align: left;
-      margin: 4px 0;
-
-      > a {
-        display: block;
-        padding: 8px 1.5em;
-      }
-
-      .router-link-active {
-        background: #fff;
-        color: #409eff;
-        text-decoration: underline;
-        font-weight: 600;
-      }
-    }
   }
 }
 
