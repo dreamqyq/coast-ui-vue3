c4ac8914c32d36bd5ae763672c65cf7ed0ab14b6
EnochQin <qyqdream@gmail.com>
2021-05-18 22:39:25 +0800

    feat: add Icon component

---
+++ lib/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg class="coast-icon" aria-hidden="true">
+    <use :xlink:href="`#coast-icon-${name}`"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+import './iconfont';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CoastIcon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
--- lib/index.ts
+++ lib/index.ts
@@ -4,4 +4,5 @@ export { default as CoTabs } from './Tabs.vue';
 export { default as CoTabPanel } from './TabPanel.vue';
 export { default as CoDialog } from './Dialog.vue';
 export { default as CoCard } from './Card.vue';
+export { default as CoIcon } from './Icon.vue';
 export { openDialog as openDialog } from './openDialog';
