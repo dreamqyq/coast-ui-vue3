3f8616679ad26cb09ef2a5ee073e3584e4357f69
EnochQin <qyqdream@gmail.com>
2021-05-19 23:48:32 +0800

    ButtonGroup: add slot validator

--- lib/button/ButtonGroup.vue
+++ lib/button/ButtonGroup.vue
@@ -6,10 +6,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from './Button.vue';
 
 export default defineComponent({
   name: 'CoastButtonGroup',
-  setup() {},
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach(tag => {
+      if (tag.type !== Button) {
+        throw new Error('CoButtonGroup 的子标签必须为 CoButton');
+      }
+    });
+  },
 });
 </script>
 
