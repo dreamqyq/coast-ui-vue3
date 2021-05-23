4045e3c0ef41f55708a93ed2e3ff1d813283c58b
EnochQin <qyqdream@gmail.com>
2021-05-23 15:57:30 +0800

    test: update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
-module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'ts',
-    'json',
-    'vue'
-  ],
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   moduleDirectories: ['node_modules', 'lib'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    '^.+\\.vue$': 'vue-jest',
+  },
+  roots: ['<rootDir>'],
+};
+
+export default config;
