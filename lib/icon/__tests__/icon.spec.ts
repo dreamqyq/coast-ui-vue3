00c2b3c3b8ac2998b451adc98bcf282575dc0660
EnochQin <qyqdream@gmail.com>
2021-05-23 22:57:11 +0800

    test: Icon add icon.spec.ts

---
+++ lib/icon/__tests__/icon.spec.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+import Icon from '../Icon.vue';
+jest.mock('../../theme-chalk/iconfont/index.js');
+
+describe('Icon', () => {
+  it('create', () => {
+    const wrapper = mount(Icon, {
+      props: { name: 'loading' },
+    });
+    expect(wrapper.classes()).toContain('coast-icon');
+    expect(wrapper.find('use').attributes('href')).toEqual('#coast-icon-loading');
+  });
+});
