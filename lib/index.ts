a6de8e9ef570e22544ce25ee980ae0129e174f46
EnochQin <qyqdream@gmail.com>
2021-05-24 21:52:30 +0800

    project: add card folder, move Card.vue to /lib/card/Card.vue

--- lib/index.ts
+++ lib/index.ts
@@ -4,6 +4,6 @@ export { default as CoButtonGroup } from './button/ButtonGroup.vue';
 export { default as CoTabs } from './Tabs.vue';
 export { default as CoTabPanel } from './TabPanel.vue';
 export { default as CoDialog } from './Dialog.vue';
-export { default as CoCard } from './Card.vue';
+export { default as CoCard } from './card/Card.vue';
 export { default as CoIcon } from './icon/Icon.vue';
 export { openDialog as openDialog } from './openDialog';
