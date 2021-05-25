0152436044a96c9314833662c28eb2b7087ba293
EnochQin <qyqdream@gmail.com>
2021-05-25 16:28:27 +0800

    project: add lib/dialog folder
    move Dialog.vue, openDialog.ts to /lib/dialog

--- lib/dialog/Dialog.vue
+++ lib/dialog/Dialog.vue
@@ -23,7 +23,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Button from './button/Button.vue';
+import Button from '../button/Button.vue';
 
 interface DialogProps {
   title?: string;
