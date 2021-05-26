b04228a298c32b7a00c7a95bfdff5814df66bf14
EnochQin <qyqdream@gmail.com>
2021-05-26 17:35:54 +0800

    project: add alias @coast to 'lib/*'
    update: jest.config.ts, tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   moduleDirectories: ['node_modules', 'lib'],
+  moduleNameMapper: {
+    '^@coast/(.*)$':  '<rootDir>/lib/$1',
+  },
   transform: {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.vue$': 'vue-jest',
