e073c31bfe2e10b10ea2eac2998b748089174baf
EnochQin <qyqdream@gmail.com>
2021-06-03 17:32:32 +0800

    Input: add style of readonly, width, label, labelRight props

--- lib/input/Input.vue
+++ lib/input/Input.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="coast-input-wrapper">
-    <input type="text" class="coast-input" :placeholder="placeholder" :disabled="disabled" />
+  <div class="coast-input-wrapper" :style="`width:${width}`">
+    <span class="coast-label left" v-if="label">{{ label }}</span>
+    <input
+      type="text"
+      class="coast-input"
+      :class="{ 'coast-input-label-left': label, 'coast-input-label-right': labelRight }"
+      :value="value"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :readonly="readonly"
+    />
+    <span class="coast-label right" v-if="labelRight">{{ labelRight }}</span>
   </div>
 </template>
 
@@ -10,12 +20,32 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'CoastInput',
   props: {
+    value: {
+      type: [String, Number],
+      required: true,
+    },
     placeholder: {
       type: String,
       required: false,
     },
     disabled: {
       type: Boolean,
+      required: false,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+    },
+    width: {
+      type: String,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: false,
+    },
+    labelRight: {
+      type: String,
       required: false,
     },
   },
@@ -28,8 +58,10 @@ $height: 36px;
 $color: #000;
 $borderColor: #eaeaea;
 $borderFocusColor: #666667;
-$boderHoverColor: #666;
+$selectionBGColor: #6affe0;
 $fontSize: 14px;
+$disabledBGColor: #fafafa;
+$disabledColor: #eaeaea;
 
 .coast-input-wrapper {
   font-size: $fontSize;
@@ -39,6 +71,30 @@ $fontSize: 14px;
   flex: 1 1 0%;
   user-select: none;
   margin: 4px;
+
+  .coast-label {
+    height: $height;
+    display: inline-flex;
+    vertical-align: middle;
+    align-items: center;
+    padding: 0 8px;
+    font-size: inherit;
+    background-color: #fafafa;
+    color: #888;
+    border-top: 1px solid #eaeaea;
+    border-bottom: 1px solid #eaeaea;
+
+    &.left {
+      border-left: 1px solid #eaeaea;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+    }
+    &.right {
+      border-right: 1px solid #eaeaea;
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    }
+  }
 
   .coast-input {
     font-family: inherit;
@@ -55,6 +111,26 @@ $fontSize: 14px;
     width: 100%;
     min-width: 0;
 
+    &.coast-input-label-left {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+
+    &.coast-input-label-right {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+
+    &::selection {
+      background-color: $selectionBGColor;
+      color: $color;
+    }
+
+    &::-moz-selection {
+      background: $selectionBGColor;
+      color: $color;
+    }
+
     &::placeholder {
       color: #757575;
     }
@@ -65,8 +141,8 @@ $fontSize: 14px;
 
     &:disabled {
       cursor: not-allowed;
-      background-color: rgb(250, 250, 250);
-      color: rgb(234, 234, 234);
+      background-color: $disabledBGColor;
+      color: $disabledColor;
     }
   }
 }
