d4d9a0507a931edfbd207501ca0c2acec7fcdab5
EnochQin <qyqdream@gmail.com>
2021-06-09 17:51:01 +0800

    examples: combing Dialog demos

--- src/examples/Dialog/index.vue
+++ src/examples/Dialog/index.vue
@@ -1,26 +1,26 @@
 <template>
   <h1>Dialog 文档</h1>
-  <Demo :component="DialogDemo1" />
-  <Demo :component="DialogDemo2" />
-  <Demo :component="DialogDemo3" />
-  <Demo :component="DialogDemo4" />
+  <Demo :component="DialogDefaultDemo" />
+  <Demo :component="DialogOverlayClosableDemo" />
+  <Demo :component="DialogConfirmCancelDemo" />
+  <Demo :component="DialogOpenDialogDemo" />
 </template>
 
 <script lang="ts">
 import Demo from '../../components/Demo.vue';
-import DialogDemo1 from './Dialog1.demo.vue';
-import DialogDemo2 from './Dialog2.demo.vue';
-import DialogDemo3 from './Dialog3.demo.vue';
-import DialogDemo4 from './Dialog4.demo.vue';
+import DialogDefaultDemo from './Dialog.default.demo.vue';
+import DialogOverlayClosableDemo from './Dialog.overlayClosable.demo.vue';
+import DialogConfirmCancelDemo from './Dialog.confirm-cancel.demo.vue';
+import DialogOpenDialogDemo from './Dialog.openDialog.demo.vue';
 
 export default {
   components: { Demo },
   setup() {
     return {
-      DialogDemo1,
-      DialogDemo2,
-      DialogDemo3,
-      DialogDemo4,
+      DialogDefaultDemo,
+      DialogOverlayClosableDemo,
+      DialogConfirmCancelDemo,
+      DialogOpenDialogDemo,
     };
   },
 };
