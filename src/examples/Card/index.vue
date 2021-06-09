8c16bde3cdeacba035de7ccb39816ddc41f8864d
EnochQin <qyqdream@gmail.com>
2021-06-09 17:52:01 +0800

    examples: combing Card demos

--- src/examples/Card/index.vue
+++ src/examples/Card/index.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
     <h1>Card 文档</h1>
-    <Demo :component="CardDemo1" />
-    <Demo :component="CardDemo2" />
-    <Demo :component="CardDemo3" />
-    <Demo :component="CardDemo4" />
-    <Demo :component="CardDemo5" />
+    <Demo :component="CardDefaultDemo" />
+    <Demo :component="CardHoverableDemo" />
+    <Demo :component="CardShadowDemo" />
+    <Demo :component="CardTypeDemo" />
+    <Demo :component="CardSlotDemo" />
   </div>
 </template>
 
 <script lang="ts">
 import Demo from '../../components/Demo.vue';
-import CardDemo1 from './Card1.demo.vue';
-import CardDemo2 from './Card2.demo.vue';
-import CardDemo3 from './Card3.demo.vue';
-import CardDemo4 from './Card4.demo.vue';
-import CardDemo5 from './Card5.demo.vue';
+import CardDefaultDemo from './Card.default.demo.vue';
+import CardHoverableDemo from './Card.hoverable.demo.vue';
+import CardShadowDemo from './Card.shadow.demo.vue';
+import CardTypeDemo from './Card.type.demo.vue';
+import CardSlotDemo from './Card.slot.demo.vue';
+
 export default {
   components: { Demo },
   setup() {
     return {
-      CardDemo1,
-      CardDemo2,
-      CardDemo3,
-      CardDemo4,
-      CardDemo5,
+      CardDefaultDemo,
+      CardHoverableDemo,
+      CardShadowDemo,
+      CardTypeDemo,
+      CardSlotDemo,
     };
   },
 };
