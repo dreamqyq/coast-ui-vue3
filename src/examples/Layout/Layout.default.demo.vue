6694afa70605317c75ef51c025a04eb9de6c4fcb
EnochQin <qyqdream@gmail.com>
2021-06-14 00:32:16 +0800

    example: add Layout demo

---
+++ src/examples/Layout/Layout.default.demo.vue
@@ -0,0 +1,94 @@
+<demo>
+  <title>常见页面布局</title>
+  <desc>Layout：外层容器。当子元素中包含 Aside 时，全部子元素会水平左右排列，否则会垂直上下排列。Header：顶栏容器。Aside：侧边栏容器。Content：主要区域容器。Footer：底栏容器。</desc>
+</demo>
+
+<template>
+  <co-layout>
+    <co-header>Header</co-header>
+    <co-content>Content</co-content>
+  </co-layout>
+
+  <co-layout>
+    <co-header>Header</co-header>
+    <co-content>Content</co-content>
+    <co-footer>Footer</co-footer>
+  </co-layout>
+
+  <co-layout>
+    <co-aside width="200px">Aside</co-aside>
+    <co-content>Content</co-content>
+  </co-layout>
+
+  <co-layout>
+    <co-header>Header</co-header>
+    <co-layout>
+      <co-aside width="200px">Aside</co-aside>
+      <co-content>Content</co-content>
+    </co-layout>
+  </co-layout>
+
+  <co-layout>
+    <co-header>Header</co-header>
+    <co-layout>
+      <co-aside width="200px">Aside</co-aside>
+      <co-layout>
+        <co-content>Content</co-content>
+        <co-footer>Footer</co-footer>
+      </co-layout>
+    </co-layout>
+  </co-layout>
+
+  <co-layout>
+    <co-aside width="200px">Aside</co-aside>
+    <co-layout>
+      <co-header>Header</co-header>
+      <co-content>Content</co-content>
+    </co-layout>
+  </co-layout>
+
+  <co-layout>
+    <co-aside width="100px">Aside</co-aside>
+    <co-layout>
+      <co-header height="70px">Header</co-header>
+      <co-content>Content</co-content>
+      <co-footer height="50px">Footer</co-footer>
+    </co-layout>
+  </co-layout>
+</template>
+
+<script lang="ts">
+import { CoAside, CoFooter, CoHeader, CoContent, CoLayout } from 'coast-ui-vue3';
+
+export default {
+  components: { CoAside, CoFooter, CoHeader, CoContent, CoLayout },
+};
+</script>
+
+<style lang="scss" scoped>
+.demo-component > .coast-layout {
+  margin-bottom: 40px;
+  color: #fff;
+  text-align: center;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  .coast-header,
+  .coast-footer {
+    background: #7dbcea;
+    line-height: 60px;
+  }
+  .coast-content {
+    background: #108ee9;
+    line-height: 120px;
+  }
+  .coast-aside {
+    background: #3ba0e9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/examples/Layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Layout 文档</h1>
+    <Demo :component="LayoutDefaultDemo" />
+  </div>
+</template>
+
+<script lang="ts">
+import LayoutDefaultDemo from './Layout.default.demo.vue';
+import Demo from '../../components/Demo.vue';
+
+export default {
+  components: { Demo },
+  setup() {
+    return {
+      LayoutDefaultDemo,
+    };
+  },
+};
+</script>
