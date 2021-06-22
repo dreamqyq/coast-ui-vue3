8332eac6f94f0547eb12648e5ed09bad9a661b03
EnochQin <qyqdream@gmail.com>
2021-06-22 23:26:04 +0800

    example: add Toast.default.demo

---
+++ src/examples/Toast/Toast.default.demo.vue
@@ -0,0 +1,26 @@
+<demo>
+  <title>常规使用</title>
+  <desc></desc>
+</demo>
+
+<template>
+  <co-button @click="onClick">打开 toast</co-button>
+</template>
+
+<script lang="ts">
+import { CoToast, CoButton } from 'coast-ui-vue3';
+
+export default {
+  components: { CoToast, CoButton },
+  setup() {
+    const onClick = () => {
+      CoToast({ text: 'Hello World' });
+    };
+
+    return { onClick };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/examples/Toast/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Layout 文档</h1>
+    <Demo :component="ToastDefaultDemo" />
+  </div>
+</template>
+
+<script lang="ts">
+import ToastDefaultDemo from './Toast.default.demo.vue';
+import Demo from '../../components/Demo.vue';
+
+export default {
+  components: { Demo },
+  setup() {
+    return {
+      ToastDefaultDemo,
+    };
+  },
+};
+</script>
