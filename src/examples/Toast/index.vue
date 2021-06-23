9328d5402d4911760b5e87d11702b8c54452564e
EnochQin <qyqdream@gmail.com>
2021-06-23 22:01:37 +0800

    example: add Toast.center.demo

--- src/examples/Toast/index.vue
+++ src/examples/Toast/index.vue
@@ -3,12 +3,14 @@
     <h1>Toast 文档</h1>
     <Demo :component="ToastDefaultDemo" />
     <Demo :component="ToastTypeDemo" />
+    <Demo :component="ToastCenterDemo" />
   </div>
 </template>
 
 <script lang="ts">
 import ToastDefaultDemo from './Toast.default.demo.vue';
 import ToastTypeDemo from './Toast.type.demo.vue';
+import ToastCenterDemo from './Toast.center.demo.vue';
 import Demo from '../../components/Demo.vue';
 
 export default {
@@ -17,6 +19,7 @@ export default {
     return {
       ToastDefaultDemo,
       ToastTypeDemo,
+      ToastCenterDemo
     };
   },
 };
