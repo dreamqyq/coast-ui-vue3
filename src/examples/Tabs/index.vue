5c22ea3f4bff3db0bd84e8add0e9e02258d0075f
EnochQin <qyqdream@gmail.com>
2021-07-25 17:12:17 +0800

    example(Tabs): add Tabs.event.demo.vue

--- src/examples/Tabs/index.vue
+++ src/examples/Tabs/index.vue
@@ -6,15 +6,13 @@
 import DemoList from '../../components/Demo/DemoList.vue';
 import TabsDefaultDemo from './Tabs.default.demo.vue';
 import TabsDisabledDemo from './Tabs.disabled.demo.vue';
+import TabsEventDemo from './Tabs.event.demo.vue';
 
 export default {
   components: { DemoList },
   setup() {
     return {
-      components: [
-        TabsDefaultDemo,
-        TabsDisabledDemo,
-      ],
+      components: [TabsDefaultDemo, TabsDisabledDemo, TabsEventDemo],
     };
   },
 };
