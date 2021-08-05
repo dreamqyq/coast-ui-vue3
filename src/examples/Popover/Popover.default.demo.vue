6428be98c2dd5f7c2ece51b5dad7a9802d6797d8
EnochQin <qyqdream@gmail.com>
2021-08-05 21:42:15 +0800

    example: add Popover.default.demo

---
+++ src/examples/Popover/Popover.default.demo.vue
@@ -0,0 +1,15 @@
+<demo>
+  <title>常规用法</title>
+</demo>
+
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { CoPopover } from 'coast-ui-vue3';
+
+export default defineComponent({
+  components: { CoPopover },
+  setup() {},
+});
+</script>
---
+++ src/examples/Popover/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <DemoList :components="components" title="Popover 文档" />
+</template>
+
+<script lang="ts">
+import DemoList from '../../components/Demo/DemoList.vue';
+import PopoverDefaultDemo from './Popover.default.demo.vue';
+
+export default {
+  components: { DemoList },
+  setup() {
+    return {
+      components: [PopoverDefaultDemo],
+    };
+  },
+};
+</script>
