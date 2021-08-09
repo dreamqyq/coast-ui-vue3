4ef0915fddefe22787191f13920d2110a0a03cfb
EnochQin <qyqdream@gmail.com>
2021-08-09 23:25:50 +0800

    example: update Popover.default.demo: different trigger example

--- src/examples/Popover/Popover.default.demo.vue
+++ src/examples/Popover/Popover.default.demo.vue
@@ -1,15 +1,39 @@
 <demo>
   <title>常规用法</title>
+  <desc>支持不同的激活方式，包含 hover, click, focus</desc>
 </demo>
 
-<template></template>
+<template>
+  <co-popover trigger="hover">
+    <template #content>
+      这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。
+    </template>
+    <co-button>hover 激活</co-button>
+  </co-popover>
+  <co-popover trigger="click">
+    <co-button>click 激活</co-button>
+    <template #content>
+      这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。
+    </template>
+  </co-popover>
+  <co-popover trigger="focus">
+    <co-button>focus 激活</co-button>
+    <template #content>
+      这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。
+    </template>
+  </co-popover>
+</template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { CoPopover } from 'coast-ui-vue3';
+import { CoButton, CoPopover } from 'coast-ui-vue3';
 
-export default defineComponent({
-  components: { CoPopover },
-  setup() {},
-});
+export default {
+  components: { CoButton, CoPopover },
+};
 </script>
+
+<style lang="scss" scoped>
+.coast-button {
+  margin-right: 20px;
+}
+</style>
