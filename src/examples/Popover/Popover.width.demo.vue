e1185da2b28767bfbfee602f6044f5e0fce3aac1
EnochQin <qyqdream@gmail.com>
2021-08-10 23:36:00 +0800

    example(Popover): add Popover.width.demo

---
+++ src/examples/Popover/Popover.width.demo.vue
@@ -0,0 +1,29 @@
+<demo>
+  <title>支持 width</title>
+  <desc>width 类型可为 number 或 string。传入数字，宽度单位为 px</desc>
+</demo>
+
+<template>
+  <co-popover position="top" trigger="hover" width="30%">
+    <template #content> 这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。 </template>
+    <co-button>width: 30%</co-button>
+  </co-popover>
+  <co-popover position="right" trigger="hover" :width="150">
+    <template #content> 这是一段文字，很高兴你看见了它。这是一段文字，很高兴你看见了它。 </template>
+    <co-button>width: 150px</co-button>
+  </co-popover>
+</template>
+
+<script lang="ts">
+import { CoButton, CoPopover } from 'coast-ui-vue3';
+
+export default {
+  components: { CoButton, CoPopover },
+};
+</script>
+
+<style lang="scss" scoped>
+.coast-button {
+  margin-right: 20px;
+}
+</style>
--- src/examples/Popover/index.vue
+++ src/examples/Popover/index.vue
@@ -6,12 +6,13 @@
 import DemoList from '../../components/Demo/DemoList.vue';
 import PopoverDefaultDemo from './Popover.default.demo.vue';
 import PopoverPositionDemo from './Popover.position.demo.vue';
+import PopoverWidthDemo from './Popover.width.demo.vue';
 
 export default {
   components: { DemoList },
   setup() {
     return {
-      components: [PopoverDefaultDemo, PopoverPositionDemo],
+      components: [PopoverDefaultDemo, PopoverPositionDemo, PopoverWidthDemo],
     };
   },
 };
