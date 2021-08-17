a1b869a4614c45d9cea9cf3a8ef8ad1e3992b223
EnochQin <qyqdream@gmail.com>
2021-08-18 00:25:56 +0800

    test(Popover): add 'ceate', 'trigger event' unit tests

---
+++ lib/popover/__tests__/popover.spec.ts
@@ -0,0 +1,95 @@
+import { getBodyElement } from '@coast/__tests__/utils/utils';
+import { mount, VueWrapper } from '@vue/test-utils';
+import { PopoverProps, TriggerType } from './../popover.d';
+import Popover from '../Popover.vue';
+import { ComponentPublicInstance } from 'vue';
+
+const AXIOM = 'Tomorrow will be even better';
+
+const _mount = (props: PopoverProps = {}) => {
+  const bindTrigger = props.trigger ? `trigger="${props.trigger}"` : '';
+  return mount({
+    components: { 'co-popover': Popover },
+    template: `
+      <co-popover ${bindTrigger}>
+        <template #content>${AXIOM}</template>
+        <button>button</button>
+      </co-popover>
+    `,
+  });
+};
+
+const expectPopoverVmVisible = (wrapper: VueWrapper<ComponentPublicInstance>, visible: boolean) => {
+  const { vm } = wrapper.findComponent(Popover);
+  expect(vm.visible).toBe(visible);
+};
+
+const expectBodyIsEmpty = (wrapper: VueWrapper<ComponentPublicInstance>) => {
+  const { bodyChildren } = getBodyElement();
+  expect(bodyChildren.length).toBe(0);
+  expectPopoverVmVisible(wrapper, false);
+};
+
+const expectPopoverExist = (wrapper: VueWrapper<ComponentPublicInstance>) => {
+  const { bodyChildren, lastElementChild } = getBodyElement();
+  expect(bodyChildren.length).toBe(1);
+  expect(lastElementChild.className).toContain('coast-popover');
+  expectPopoverVmVisible(wrapper, true);
+};
+
+const handleTriggerEvent = async (
+  trigger: TriggerType,
+  wrapper: VueWrapper<ComponentPublicInstance>,
+  index: number,
+) => {
+  const eventMap = {
+    click: {
+      events: ['click', 'click'],
+      selector: 'button',
+    },
+    hover: {
+      events: ['mouseenter', 'mouseleave'],
+      selector: '.coast-popover-slot',
+    },
+    focus: {
+      events: ['focusin', 'focusout'],
+      selector: 'button',
+    },
+  };
+  const currentEvent = eventMap[trigger];
+  const element = wrapper.find(currentEvent.selector);
+  await element.trigger(currentEvent.events[index]);
+};
+
+const expectPopoverTriggerCorrectly = async (trigger: TriggerType) => {
+  const wrapper = _mount({ trigger });
+  expectBodyIsEmpty(wrapper);
+
+  await handleTriggerEvent(trigger, wrapper, 0);
+  expectPopoverExist(wrapper);
+
+  await handleTriggerEvent(trigger, wrapper, 1);
+  expectBodyIsEmpty(wrapper);
+
+  wrapper.unmount();
+};
+
+describe('Popover', () => {
+  it('create', async () => {
+    const wrapper = _mount();
+    expectBodyIsEmpty(wrapper);
+    await handleTriggerEvent('click', wrapper, 0);
+    const { bodyChildren: afterBody, lastElementChild } = getBodyElement();
+    expect(afterBody.length).toBe(1);
+    expect(lastElementChild.className).toContain('coast-popover');
+    expect(lastElementChild.className).toContain('coast-popover-top');
+    wrapper.unmount();
+  });
+
+  it('trigger event', async () => {
+    const triggerList = ['click', 'hover', 'focus'] as const;
+    for (const trigger of triggerList) {
+      await expectPopoverTriggerCorrectly(trigger);
+    }
+  });
+});
--- lib/popover/popover.d.ts
+++ lib/popover/popover.d.ts
@@ -2,7 +2,7 @@ export type TriggerType = 'click' | 'hover' | 'focus';
 export type PositionType = 'top' | 'left' | 'right' | 'bottom';
 
 export interface PopoverProps {
-  trigger: TriggerType;
-  position: PositionType;
-  width: number | string;
+  trigger?: TriggerType;
+  position?: PositionType;
+  width?: number | string;
 }
