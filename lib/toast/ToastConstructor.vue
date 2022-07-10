a4bbefe1c426c4da03c6813844302c6e77a7cf71
EnochQin <qyqdream@gmail.com>
2022-07-10 19:10:13 +0800

    refactor(Toast): separate style file

--- lib/toast/ToastConstructor.vue
+++ lib/toast/ToastConstructor.vue
@@ -12,89 +12,85 @@
 </template>
 
 <script lang="ts">
+export default { name: 'CoastToast' };
+</script>
+
+<script lang="ts" setup>
 import type { PropType } from 'vue';
 import { computed, defineComponent, onMounted, ref } from 'vue';
 import type { ToastType, ToastProps } from './toast.d';
 import Icon from '../icon/Icon.vue';
 
-export default defineComponent({
-  name: 'CoastToast',
-  components: { Icon },
-  props: {
-    text: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    type: {
-      type: String as PropType<ToastType>,
-      required: false,
-      default: 'normal',
-      validator: (val: string) =>
-        ['normal', 'secondary', 'success', 'warning', 'error'].includes(val),
-    },
-    delay: {
-      type: Number,
-      required: false,
-      default: 3,
-    },
-    center: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    showClose: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    id: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    top: {
-      type: Number,
-      default: 10,
-      required: false,
-    },
+const props = defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: '',
   },
-  emits: ['destroy'],
-  setup(props: ToastProps, { emit }) {
-    const visible = ref(false);
-    const timer = ref(null);
-
-    const classes = computed(() => [
-      'coast-toast',
-      {
-        'coast-toast-center': props.center,
-        [`coast-toast-${props.type}`]: props.type,
-      },
-    ]);
-
-    onMounted(() => {
-      visible.value = true;
-      startTimer();
-    });
-
-    const handleClose = () => {
-      visible.value = false;
-      emit('destroy');
-    };
-    const startTimer = () => {
-      if (props.delay > 0) {
-        timer.value = setTimeout(() => {
-          handleClose();
-        }, props.delay * 1000);
-      }
-    };
-
-    const clearTimer = () => {
-      clearTimeout(timer.value);
-      timer.value = null;
-    };
-
-    return { visible, classes, handleClose, startTimer, clearTimer };
+  type: {
+    type: String as PropType<ToastType>,
+    required: false,
+    default: 'normal',
+    validator: (val: string) =>
+      ['normal', 'secondary', 'success', 'warning', 'error'].includes(val),
+  },
+  delay: {
+    type: Number,
+    required: false,
+    default: 3,
+  },
+  center: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  showClose: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  id: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  top: {
+    type: Number,
+    default: 10,
+    required: false,
   },
 });
+const emits = defineEmits(['destroy']);
+const visible = ref(false);
+const timer = ref(null);
+
+const classes = computed(() => [
+  'coast-toast',
+  {
+    'coast-toast-center': props.center,
+    [`coast-toast-${props.type}`]: props.type,
+  },
+]);
+
+onMounted(() => {
+  visible.value = true;
+  startTimer();
+});
+
+const handleClose = () => {
+  visible.value = false;
+  emits('destroy');
+};
+const startTimer = () => {
+  if (props.delay > 0) {
+    timer.value = setTimeout(() => {
+      handleClose();
+    }, props.delay * 1000);
+  }
+};
+
+const clearTimer = () => {
+  clearTimeout(timer.value);
+  timer.value = null;
+};
 </script>
